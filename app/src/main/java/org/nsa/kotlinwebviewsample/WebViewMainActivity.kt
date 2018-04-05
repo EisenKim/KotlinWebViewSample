28b5f4e1e6e098a7acc2269272a7b7a32227802b
EisenKim <daseisen@gmail.com>
2018-04-05 09:35:35 +0900

    Initial commit

---
+++ app/src/main/java/org/nsa/kotlinwebviewsample/WebViewMainActivity.kt
@@ -0,0 +1,12 @@
+package org.nsa.kotlinwebviewsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WebViewMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view_main)
+    }
+}
